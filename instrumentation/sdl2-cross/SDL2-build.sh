e89ce128e3e54dab26ffafd5a16788d0c104d7ad
Håvard Bjerke <h0wy36@gmail.com>
2017-06-02 14:13:35 +0200

    - Add cross-builder with more options

--- instrumentation/sdl2-cross/SDL2-build.sh
+++ instrumentation/sdl2-cross/SDL2-build.sh
@@ -8,6 +8,16 @@ export CXX=${CROSS}-g++
 export AS=${CROSS}-as
 export LD=${CROSS}-ld
 
-${SRC_DIR}/configure --host ${CROSS} --disable-shared --prefix=$(pwd)/out
+${SRC_DIR}/configure \
+	--host ${CROSS} \
+	--disable-shared \
+        --disable-atomic \
+        --disable-filesystem \
+        --disable-threads \
+        --disable-timers \
+        --disable-audio \
+        --disable-loadso \
+        --disable-cpuinfo \
+	--prefix=$(pwd)/out
 
 make -j$(nproc)
